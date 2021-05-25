b084f65d0510553d6e1d01e27dc342c6ef599a41
ViniciusTaborda <vinicius.costa@combudata.com>
2021-05-25 09:17:05 -0300

    Added tests for user endpoint

--- app/core/models.py
+++ app/core/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
 
 class UserManager(BaseUserManager):
 
-    def create_user(self, email, password=None, **extra_fields):
+    def create_user(self, email, password, **extra_fields):
 
         if not email:
             raise ValueError('Users must have an email address')
